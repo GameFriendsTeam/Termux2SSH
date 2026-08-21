a6c6afe50e426420f1153363df5aaeb84bcb2994
Savin1play140 <savingame1140@gmail.com>
2026-08-21 21:45:57 +0300

    Fix Russian translation

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "ru.gft.termux2ssh"
         minSdk = 24
         targetSdk = 37
-        versionCode = 4
-        versionName = "1.3"
+        versionCode = 5
+        versionName = "1.3.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
